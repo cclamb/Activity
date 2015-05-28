420f879b42d121ef8ca6994b3f382e8ce45baa2c
Chris Lamb <cclamb@ece.unm.edu>
2015-05-28 07:02:46 -0600

    work

--- app/src/main/java/com/sampa/activity/ListActivityFragment.java
+++ app/src/main/java/com/sampa/activity/ListActivityFragment.java
@@ -1,18 +1,16 @@
 package com.sampa.activity;
 
-import android.app.ListFragment;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public final class ListActivityFragment extends ListFragment {
+public class ListActivityFragment extends Fragment {
 
     public ListActivityFragment() {
     }
@@ -20,14 +18,6 @@ public final class ListActivityFragment extends ListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_list, container, false);
-        populateList(view);
-        return view;
-    }
-
-    private void populateList(View view) {
-        // ListView listView = (ListView) view.findViewById(R.id.listView);
-
+        return inflater.inflate(R.layout.fragment_list, container, false);
     }
 }
